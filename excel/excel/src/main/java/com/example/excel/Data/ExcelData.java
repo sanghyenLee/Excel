ba600bf0090cd5c631bb7e4a8a9da4b33671e17c
sanghyenLee <alikcz83@gmail.com>
2022-12-14 14:16:32 +0900

    Initial commit

---
+++ excel/excel/src/main/java/com/example/excel/Data/ExcelData.java
@@ -0,0 +1,14 @@
+package com.example.excel.Data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExcelData {
+    private Integer num;
+
+    private String name;
+
+    private String email;
+}
